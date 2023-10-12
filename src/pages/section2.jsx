9676c54ad15df345290879fc73b39339a30cb596
marwan ayman <96918247+marwanayman128@users.noreply.github.com>
2023-10-11 17:18:32 -0700

    Update section2.jsx

--- src/pages/section2.jsx
+++ src/pages/section2.jsx
@@ -1,4 +1,4 @@
-
+import CheckIcon from '@mui/icons-material/Check';
 export default function section2() {
     return (
 
@@ -12,19 +12,19 @@ export default function section2() {
                             <p>ASP Building services are amongst the leading building contractors in Sydney that has been established over twenty-five years and has been involved in various kinds of constructions and buildings such as new buildings as well as renovations and additions and extensions with simple or complex plans and designs. Our building contractors’ jobs are always constantly monitored by qualified inspectors to ensure the highest quality jobs. Our number one priority is our customer’s satisfaction, we are never happy until our clients are. ASP Building Services have been around in Sydney for 29 years, and we always keep your satisfaction and trust as our main aim and priority.</p>
                             <ul className="listext list-unstyled mb-30">
                                 <li>
-                                    <div className="listext-icon"> <i className="asp-d-check" /> </div>
+                                    <div className="listext-icon"> <CheckIcon style={{ color: "#fff", backgroundColor: "#ED5521", borderRadius: "50%" }} /> </div>
                                     <div className="listext-text">
                                         <p>Over 25 years of experience</p>
                                     </div>
                                 </li>
                                 <li>
-                                    <div className="listext-icon"> <i className="asp-d-check" /> </div>
+                                    <div className="listext-icon"> <CheckIcon style={{ color: "#fff", backgroundColor: "#ED5521", borderRadius: "50%" }} /> </div>
                                     <div className="listext-text">
                                         <p>100+ successfully executed projects</p>
                                     </div>
                                 </li>
                                 <li>
-                                    <div className="listext-icon"> <i className="asp-d-check" /> </div>
+                                    <div className="listext-icon"> <CheckIcon style={{ color: "#fff", backgroundColor: "#ED5521", borderRadius: "50%" }} /> </div>
                                     <div className="listext-text">
                                         <p>Exceptional work quality</p>
                                     </div>
@@ -33,7 +33,7 @@ export default function section2() {
                             <div className="line-dec" />
                         </div>
                         <div className="col-md-6">
-                            <div className="about-img"> <img src="https://www.aspbuilding.com.au/data/uploads/images/about/about1.jpg" alt />
+                            <div className="about-img"> <img src="https://res.cloudinary.com/dpmu17ns4/image/upload/v1697066982/ASP/d1c1g4yfnz4rhkwenlpx.jpg" alt="dummy" />
                                 <div className="about-img-hotifer">
                                     <p>Our 25 years working experience make a different construction building.</p>
                                 </div>
